a045ee33b672fcb2cef5d68155fe36a5becf6ac0
JG <jegutman@gmail.com>
2017-09-04 16:23:41 -0500

    start making class to do internal repr

--- deck_manager.py
+++ deck_manager.py
@@ -4,6 +4,31 @@ import json
 import sys
 from json_cards_to_python import cards_by_id as cards
 
+class EasyDeck():
+    self.deck = None
+
+    def __init__(self, deckstring):
+        self.deck = Deck.from_deckstring(deck_string)
+
+    def __repr__(self):
+        res = ""
+        total = 0
+        deck = []
+        for i,j in deck_cards:
+            total += j
+            count, name, card_class, cost = j, cards[i]['name'], cards[i]['playerClass'], cards[i]['cost']
+            if card_class == 'NEUTRAL':
+                card_class = "ZZ_NEUTRAL"
+            deck.append([card_class, cost, name, count])
+        deck.sort()
+        last_class = ""
+        for card_class, cost, name, count in deck:
+            if card_class != last_class:
+                res += ""
+                res += card_class.replace('ZZ_', '')
+            last_class = card_class
+            res += "%-2s %-25s x%s" % (cost, name, count)
+
 def print_deck(deck_cards, cards=cards):
     total = 0
     deck = []
