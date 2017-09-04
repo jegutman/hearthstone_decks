7936a3d37f280e836d622441d307eadeb4c24d6d
jegutman <jegutman@gmail.com>
2017-09-04 12:02:28 -0500

    so I can work on my laptop

--- deck_compare.py
+++ deck_compare.py
@@ -5,3 +5,10 @@ from decks import decks
 decklist = []
 for name, code in decks:
     decklist.append((name, deck_from_string(code)))
+
+def display_deck_comparison(decks):
+    pass
+
+def display_deck_comparison_from_strings(deckstrings):
+    decks = [deck_from_string(ds) for ds in deckstrings]
+    display_deck_comparison(decks)
--- decks.py
+++ decks.py
@@ -1,7 +1,8 @@
 decks = [
 #(name, code),
 
-("Razakus (Strife)", "AAECAZ/HAh6KAe0B+wGXApwC5QTtBckG9gelCdIK0wrXCvIM+wyStAKDuwK1uwK3uwLYuwLwuwLhvwLqvwLRwQLZwQKbwgK+yAKgzgLwzwKQ0wIAAA=="),
+#("Razakus (Strife)", "AAECAZ/HAh6KAe0B+wGXApwC5QTtBckG9gelCdIK0wrXCvIM+wyStAKDuwK1uwK3uwLYuwLwuwLhvwLqvwLRwQLZwQKbwgK+yAKgzgLwzwKQ0wIAAA=="),
 ("Razakus (Pavel)",  "AAECAZ/HAh4JigH7AZAClwLlBO0FyQalCdMK1wryDPsMoawCkrQCg7sCtbsCt7sC2LsCsLwC4b8C6L8C6r8C0cEC2MEC2cEC08UCvsgC8M8CkNMCAAA="),
+("Raza (Josh)", "AAECAZ/HAh4JigH7AZAClwLlBO0FyQalCdMK1wryDPsMoawCkrQCg7sCtbsCt7sC2LsCsLwC4b8C6L8C6r8C0cEC2MEC2cECysMC08UC8M8CkNMCAAA="),
 
 ]
