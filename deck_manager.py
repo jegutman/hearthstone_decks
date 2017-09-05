2c1b08ec863690a6f952027b74cb8b3ff93155a4
JG <jegutman@gmail.com>
2017-09-04 20:26:21 -0500

    fix

--- deck_manager.py
+++ deck_manager.py
@@ -5,16 +5,18 @@ import sys
 from json_cards_to_python import cards_by_id as cards
 
 class EasyDeck():
-    self.deck = None
 
-    def __init__(self, deckstring):
-        self.deck = Deck.from_deckstring(deck_string)
+    def __init__(self, deckstring, name = ""):
+        self.deck = Deck.from_deckstring(deckstring)
+        self.name = name
+        self.deckstring = deckstring
+        assert sum([c[1] for c in self.deck.cards]) == 30, self.deck.cards
 
     def __repr__(self):
         res = ""
         total = 0
         deck = []
-        for i,j in deck_cards:
+        for i,j in self.deck.cards:
             total += j
             count, name, card_class, cost = j, cards[i]['name'], cards[i]['playerClass'], cards[i]['cost']
             if card_class == 'NEUTRAL':
@@ -28,6 +30,22 @@ class EasyDeck():
                 res += card_class.replace('ZZ_', '')
             last_class = card_class
             res += "%-2s %-25s x%s" % (cost, name, count)
+        return res
+
+    #def get_class(self):
+    #    return self.deck.class()
+    
+    def get_original_code(self):
+        return self.deckstring
+
+    def get_card_codes(self):
+        return self.deck.cards
+
+    def get_card_names(self):
+        res = []
+        for i, j in self.deck.cards:
+            res.append((j, cards[i]))
+        return res
 
 def print_deck(deck_cards, cards=cards):
     total = 0
