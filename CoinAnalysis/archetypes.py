3c29fbb4bebc1c2f9e9e85a5c5975c216545c296
JG <jegutman@gmail.com>
2017-09-24 19:58:18 -0500

    some updates to allow analysis of aggro

---
+++ CoinAnalysis/archetypes.py
@@ -0,0 +1,40 @@
+aggro = [
+    # only including large enough sample sizes
+    'Evolve Shaman',
+    'Midrange Hunter',
+    'Deathrattle Hunter',
+    'Secret Mage',
+    'Prince Rogue',
+    'Pirate Warrior',
+    'Aggro Druid',
+    'Zoolock',
+    'Elemental Rogue',
+    'Murloc Paladin',
+    'Grimestreet Paladin',
+]
+other = [
+    # should be aggro, but low sample
+    'Tempo Mage',
+    'Aggro Paladin',
+
+    'Highlander Priest',
+    'Highlander Mage',
+    'Dead Man Warrior',
+    'Control Paladin',
+    'Mill Rogue',
+    'Highlander Warlock',
+    'Quest Mage',
+    'Control Mage',
+    'Big Druid',
+    'Taunt Druid',
+    'Demon Control Warlock',
+    'Resurrect Priest',
+    'Dragon Priest',
+    'Silence Priest',
+    'Handlock',
+    'Miracle Rogue',
+    'Jade Druid',
+    'Quest Rogue',
+    'Quest Warrior',
+    'Enrage Warrior',
+]
--- CoinAnalysis/import.py
+++ CoinAnalysis/import.py
@@ -1,3 +1,4 @@
+from archetypes import aggro
 archetypes = []
 data = {}
 line_data = []
@@ -8,8 +9,10 @@ with open('CoinData.csv') as f:
             continue
         tmp = line.strip().split(',')
         deck_a, deck_b, first, pct, games = tmp
+        if deck_a not in aggro or deck_b not in aggro: continue
         for d in (deck_a, deck_b):
             if d not in archetypes:
+                assert d != '10', line
                 archetypes.append(d)
             if d not in data:
                 data[d] = {}
@@ -21,3 +24,29 @@ with open('CoinData.csv') as f:
         games = int(games)
         data[deck_a][deck_b][first] = (pct, games)
         line_data.append((deck_a, deck_b, first, pct, games))
+
+diffs = {}
+deck_stats = {}
+games_count = {}
+for deck_a, deck_b, first, pct, games in line_data:
+    key = (deck_a, first)
+    deck_stats[key] = deck_stats.get(key, 0) + int(round(pct * games / 100))
+    games_count[key] = games_count.get(key, 0) + games
+
+overall = []
+for i in archetypes:
+    pct_1 = round(float(deck_stats[(i, 1)] / games_count[(i,1)]) * 100, 1)
+    pct_0 = round(float(deck_stats[(i, 0)] / games_count[(i,0)]) * 100, 1)
+    #min_g = min(games_count[(i,1)], games_count[(i,0)])
+    g_1 = games_count[(i,1)]
+    g_0 = games_count[(i,0)]
+    diff = round(pct_1 - pct_0, 1)
+    #print("%-25s" % i, pct_1, pct_0, "%5.1f" % diff, "%6s" % min_g)
+    #overall.append((i, pct_1, pct_0, diff, min_g))
+    overall.append((i, pct_1, pct_0, diff, g_1, g_0))
+
+#for i, pct_1, pct_0, diff, min_g in sorted(overall, key=lambda x:x[-2], reverse=True):
+#    print("%-25s" % i, pct_1, pct_0, "%5.1f" % diff, "%6s" % min_g)
+
+for i, pct_1, pct_0, diff, g_1, g_0 in sorted(overall, key=lambda x:x[3], reverse=True):
+    print("%-25s" % i, pct_1, pct_0, "%5.1f" % diff, "%6s" % g_1, "%6s" % g_0)
