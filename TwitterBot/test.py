5dd57349a7f5def6cab7d2f3aa452291a931a39e
JG <jegutman@gmail.com>
2018-04-16 16:52:52 -0500

    remove key for twitter and reset key

--- TwitterBot/test.py
+++ TwitterBot/test.py
@@ -25,13 +25,14 @@ iterator = twitter_stream.statuses.sample()
 # Here we set it to stop after getting 1000 tweets. 
 # You don't have to set it to stop, but can continue running 
 # the Twitter API to collect data for days or even longer. 
-tweet_count = 1000
+tweet_count = 1
 for tweet in iterator:
     tweet_count -= 1
     # Twitter Python Tool wraps the data returned by Twitter 
     # as a TwitterDictResponse object.
     # We convert it back to the JSON format to print/score
-    print json.dumps(tweet)  
+    #tmp_tweet = json.dumps(tweet)
+    tmp_tweet = tweet
     
     # The command below will do pretty printing for JSON data, try it out
     # print json.dumps(tweet, indent=4)
