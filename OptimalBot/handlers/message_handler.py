d7cb07a992dc7f43277e4eab235d9c83dd48a118
JG <jegutman@gmail.com>
2018-05-01 01:55:44 -0500

    fix

--- OptimalBot/handlers/message_handler.py
+++ OptimalBot/handlers/message_handler.py
@@ -152,9 +152,9 @@ class MessageHandler:
                 res = '`'
                 res += 'NA:   ' + t1 + '\n'
                 res += 'EU:   ' + t2 + '\n'
-                res += 'APAC: ' + t2 + '\n'
+                res += 'APAC: ' + t3 + '\n'
                 res += '`'
-                return res
+                await self.respond(message, res)
 
             await self.respond(message, '`' + str(end_time - datetime.now()).split('.')[0] + ' left in season`')
 
