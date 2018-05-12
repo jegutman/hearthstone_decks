f2ca5169faf868ac907da72320abcf7d3d7afb19
JG <jegutman@gmail.com>
2018-05-11 19:38:50 -0500

    updated

--- TwitterBot/test.py
+++ TwitterBot/test.py
@@ -1,8 +1,8 @@
-#  Import the necessary package to process data in JSON format
-try:
-    import json
-except ImportError:
-    import simplejson as json
+import json
+import datetime
+
+import re
+deckstring_re = re.compile('(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4}){12,}')
 
 # Import the necessary methods from "twitter" library
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
@@ -12,27 +12,31 @@ from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 from twitter_config import *
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
-# Initiate the connection to Twitter Streaming API
-twitter_stream = TwitterStream(auth=oauth)
+twitter = Twitter(auth=oauth)
 
-# Get a sample of the public data following through Twitter
-iterator = twitter_stream.statuses.sample()
 
-# Print each tweet in the stream to the screen 
-# Here we set it to stop after getting 1000 tweets. 
-# You don't have to set it to stop, but can continue running 
-# the Twitter API to collect data for days or even longer. 
-tweet_count = 1
-for tweet in iterator:
-    tweet_count -= 1
-    # Twitter Python Tool wraps the data returned by Twitter 
-    # as a TwitterDictResponse object.
-    # We convert it back to the JSON format to print/score
-    #tmp_tweet = json.dumps(tweet)
-    tmp_tweet = tweet
-    
-    # The command below will do pretty printing for JSON data, try it out
-    # print json.dumps(tweet, indent=4)
-       
-    if tweet_count <= 0:
-        break
+users_to_parse = [
+    'xixohs',
+    'rage_hs',
+    'hsprodecks',
+    'ahirun',
+    'NVD_hunterace',
+    'tylerootd',
+    'riku97hs',
+    'Furyhunterhs',
+    'fireaei',
+    'Gyong_hs',
+    'G2Thijs',
+    'liquid_hsdog',
+]
+
+for user in users_to_parse:
+    for i in twitter.statuses.user_timeline(screen_name=user, count=200, tweet_mode='extended'):
+        #print(i['full_text'])
+        deckstring_matches = deckstring_re.findall(i['full_text'])
+        if deckstring_matches:
+            time = datetime.datetime.strptime(i['created_at'][:19] + i['created_at'][-5:], "%a %b %d %H:%M:%S %Y")
+            text_time = time.strftime("%Y_%m_%d")
+            if text_time > '2018_04_11':
+                print(user, text_time, deckstring_matches)
+        #print(i['full_text'])
