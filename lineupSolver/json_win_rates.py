0bc34ab965775792b86b291448309eadbcc4abf7
J G <jegutman@gmail.com>
2018-05-29 19:22:11 +0000

    fix

--- lineupSolver/json_win_rates.py
+++ lineupSolver/json_win_rates.py
@@ -7,12 +7,12 @@ sys.path.append(basedir + '/lineupSolver')
 import json
 from config import basedir
 
-date = '20180523'
+date = '20180529'
 base = basedir
 #filename = '%(base)slineupSolver/win_rates/hsreplay%(date)s_L5_7DAYS.json' % locals()
 #filename = '%(base)slineupSolver/win_rates/hsreplay%(date)s_LONLY_1DAY.json' % locals()
-#filename = '%(base)slineupSolver/win_rates/hsreplay%(date)s_LONLY_3DAYS.json' % locals()
-filename = '%(base)slineupSolver/win_rates/hsreplay%(date)s_LONLY_7DAYS.json' % locals()
+filename = '%(base)slineupSolver/win_rates/hsreplay%(date)s_LONLY_3DAYS.json' % locals()
+#filename = '%(base)slineupSolver/win_rates/hsreplay%(date)s_LONLY_7DAYS.json' % locals()
 #filename = '%(base)slineupSolver/win_rates/hsreplay%(date)sday.json' % locals()
 #filename = '%(base)slineupSolver/win_rates/hsreplay%(date)slegend_day.json' % locals()
 #filename = '%(base)slineupSolver/win_rates/hsreplay%(date)slegend_day.json' % locals()
