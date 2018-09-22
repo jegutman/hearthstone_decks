3076aea1d967cc2707ad0331c6be3d20e3e3feba
JG <jegutman@gmail.com>
2018-09-21 22:06:34 -0500

    cn vs eu archetypes

--- TourStopLoader/CNvsEU/codes_to_archetypes.py
+++ TourStopLoader/CNvsEU/codes_to_archetypes.py
@@ -10,23 +10,23 @@ from label_archetype import label_archetype
 from deck_manager import *
 
 
-filename = 'CNvsEU.csv'
+filename = 'CNvsEUdecks.csv'
 week_file = open(filename)
 #output_file = open(filename.replace('codes', 'lineups'), 'w')
-output_file = open('CNvsEUarchetypes.csv')
+output_file = open('CNvsEUarchetypes.csv', 'w')
 lines = [line.strip() for line in week_file]
 
 output_file.write(lines[0] + '\n')
-for line in lines[1:]:
+for line in lines:
     print(line)
     tmp = line.split(',')
     player = tmp[0]
     for i in tmp[1:5]:
-        #print(country, i)
+        #print(player, i)
         label = label_archetype(EasyDeck(i))
         if not label: 
             print('x',i,'x')
             EasyDeck(i).print_deck()
     archetypes = [label_archetype(EasyDeck(i)) for i in tmp[1:10]]
     print(archetypes)
-    output_file.write(",".join([country] + archetypes) + '\n')
+    output_file.write(",".join([player] + archetypes) + '\n')
